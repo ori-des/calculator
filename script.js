faa1c3b2bd4a6850ae4396b5e396acce2514fe59
ori-des <151224246+ori-des@users.noreply.github.com>
2024-01-27 13:01:03 +0100

    Add functions

---
+++ script.js
@@ -0,0 +1,31 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, operator, secondNumber) {
+    if (operator === "+") {
+        return add(firstNumber, secondNumber);
+    } if (operator === "-") {
+        return subtract(firstNumber, secondNumber);
+    } if (operator === "*") {
+        return multiply(firstNumber, secondNumber);
+    } if (operator === "/") {
+        return divide(firstNumber, secondNumber);
+    }
+}
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
